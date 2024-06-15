3bd81040f68bea68ad79e78e4e3d5ac37d02f9e0
Fabio da Silva Fernandes <fernandesfabio@fabios-MacBook-Air.local>
2024-06-15 00:15:07 -0300

    Feito alterações na index e criado nova page conversor.html

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -18,3 +18,29 @@ window.addEventListener("scroll", function() {
     btnScroll.classList.toggle('btn--scroll', window.scrollY > 0);
     }
 )
+
+/* Açoões botões scroll, rolagem imagens */
+
+const initSlider = () => {
+    const imageList= document.querySelector('[data-slide="list"]');
+    const slideButtons = document.querySelectorAll('[data-slide="slide-btn"]');
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+    });
+}
+window.addEventListener("load", initSlider);
